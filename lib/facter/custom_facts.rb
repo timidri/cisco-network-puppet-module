98700c9fec7f5e9f3faf6720b25c7c8c1d0adb3f
Dimitri Tischenko <dimitri@puppet.com>
2019-04-09 18:18:05 +0200

    added interfaces fact

--- lib/facter/custom_facts.rb
+++ lib/facter/custom_facts.rb
@@ -1,8 +1,21 @@
+require 'cisco_node_utils'
+
 class Facter::CiscoNexus::CustomFacts
 
   def self.add_custom_facts(facts)
 
     facts['my_custom_fact'] = 'my_custom_value'
+
+    interfaces = []
+    Cisco::Interface.interfaces.each do |interface_name, nu_obj|
+      begin
+        # Some interfaces cannot or should not be managed by this type.
+        # - NVE Interfaces (managed by cisco_vxlan_vtep type)
+        next if interface_name.match(/nve/i)
+        interfaces << interface_name
+      end
+    end
+    facts['interfaces'] = interfaces
 
   end
 end
