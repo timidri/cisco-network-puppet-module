2093e9b90f9b9ee61ca2d23abb7bf44d13afc873
Dimitri Tischenko <dimitri@puppet.com>
2019-04-10 14:43:10 +0200

    added some props to intefaces custom fact

--- lib/facter/custom_facts.rb
+++ lib/facter/custom_facts.rb
@@ -1,21 +1,26 @@
 require 'cisco_node_utils'
 
 class Facter::CiscoNexus::CustomFacts
+  PROPS = [:mtu, :speed, :duplex]
 
   def self.add_custom_facts(facts)
-
     facts['my_custom_fact'] = 'my_custom_value'
 
-    interfaces = []
+    interfaces = {}
     Cisco::Interface.interfaces.each do |interface_name, nu_obj|
       begin
-        # Some interfaces cannot or should not be managed by this type.
-        # - NVE Interfaces (managed by cisco_vxlan_vtep type)
-        next if interface_name.match(/nve/i)
-        interfaces << interface_name
-      end
+          # Some interfaces cannot or should not be managed by this type.
+          # - NVE Interfaces (managed by cisco_vxlan_vtep type)
+          next if interface_name =~ /nve/i
+          state = {}
+          # Call node_utils getter for each property
+          PROPS.each do |prop|
+            state[prop] = nu_obj.send(prop)
+          end
+
+          interfaces << { interface_name: state }
+        end
     end
     facts['interfaces'] = interfaces
-
   end
 end
