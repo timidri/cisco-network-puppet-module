755140e47271d5c3d9d4f0e1426e31885876b59d
Dimitri Tischenko <dimitri@puppet.com>
2019-04-11 10:52:01 +0200

    removed test fact, formatting

--- lib/facter/custom_facts.rb
+++ lib/facter/custom_facts.rb
@@ -4,22 +4,22 @@ class Facter::CiscoNexus::CustomFacts
   PROPS = [:mtu, :speed, :duplex]
 
   def self.add_custom_facts(facts)
-    facts['my_custom_fact'] = 'my_custom_value'
+    # facts['my_custom_fact'] = 'my_custom_value'
 
     interfaces = {}
     Cisco::Interface.interfaces.each do |interface_name, nu_obj|
       begin
-          # Some interfaces cannot or should not be managed by this type.
-          # - NVE Interfaces (managed by cisco_vxlan_vtep type)
-          next if interface_name =~ /nve/i
-          state = {}
-          # Call node_utils getter for each property
-          PROPS.each do |prop|
-            state[prop] = nu_obj.send(prop)
-          end
-
-          interfaces[interface_name] = state
+        # Some interfaces cannot or should not be managed by this type.
+        # - NVE Interfaces (managed by cisco_vxlan_vtep type)
+        next if interface_name =~ /nve/i
+        state = {}
+        # Call node_utils getter for each property
+        PROPS.each do |prop|
+          state[prop] = nu_obj.send(prop)
         end
+
+        interfaces[interface_name] = state
+      end
     end
     facts['interfaces'] = interfaces
   end
