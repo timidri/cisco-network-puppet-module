8a48fb64c040d26a4117c0f940a27cb8a16e0efb
Jens Jetzork <jetzorkj@telekom.de>
2019-05-24 11:23:24 +0200

    Added HSRP group custom fact

--- lib/facter/custom_facts.rb
+++ lib/facter/custom_facts.rb
@@ -1,7 +1,8 @@
 require 'cisco_node_utils'
 
 class Facter::CiscoNexus::CustomFacts
-  PROPS = [:mtu, :speed, :duplex, :encapsulation_dot1q, :description, :ipv4_address]
+  INTERFACE_PROPS = [:mtu, :speed, :duplex, :encapsulation_dot1q, :description, :ipv4_address]
+  HSRP_PROPS = [:ipv4_vip, :preempt, :priority]
 
   def self.add_custom_facts(facts)
     # facts['my_custom_fact'] = 'my_custom_value'
@@ -14,13 +15,29 @@ class Facter::CiscoNexus::CustomFacts
         next if interface_name =~ /nve/i
         state = {}
         # Call node_utils getter for each property
-        PROPS.each do |prop|
+        INTERFACE_PROPS.each do |prop|
           state[prop] = nu_obj.send(prop)
         end
 
         interfaces[interface_name] = state
       end
     end
+    
+    hsrp_groups = {}
+    Cisco::InterfaceHsrpGroup.groups.each do |group_name, nu_obj|
+      begin
+        state = {}
+        # Call node_utils getter for each property
+        HSRP_PROPS.each do |prop|
+          state[prop] = nu_obj.send(prop)
+        end
+
+        hsrp_groups[group_name] = state
+      end
+    end
+
     facts['interfaces'] = interfaces
+    facts['hsrp'] = hsrp_groups
+
   end
 end
